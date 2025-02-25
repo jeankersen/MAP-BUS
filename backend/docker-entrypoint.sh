ffa014f9487f92329adb0175558acae5229d8d20
kersen <kersenferon@yahoo.com>
2025-02-25 16:59:07 -0500

    delete backend files

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Attendre que la base de données soit prête
+
 echo "Waiting for the database to be ready..."
 until nc -z -v -w30 db 5432; do
   echo "Waiting for database connection..."
@@ -8,14 +8,19 @@ until nc -z -v -w30 db 5432; do
 done
 echo "Database is ready!"
 
-# Appliquer les migrations
+
 echo "Applying migrations..."
+python manage.py makemigrations
 python manage.py migrate
 
-# Supprimer et recréer le superutilisateur
+
 echo "Deleting and creating superuser..."
 python create_superuser.py
 
-# Démarrer le serveur Django
+echo "Importing Calendar..."
+python manage.py import_calendar
+
+
+
 echo "Starting Django server..."
 exec python manage.py runserver 0.0.0.0:8000
